9679aac2a2e63edde3ce34de22052a7fea2e185b
RocAfella044 <mandeep.rajbhandari.a22@icp.edu.np>
2026-07-08 11:22:19 +0545

    changes

--- src/data/fixtures.js
+++ src/data/fixtures.js
@@ -1,8 +1,6 @@
 export const fixturesData = [
-  { id: 2, opponent: "Unknown", Type: 'Unknown', date: "", time: "Not scheduled", venue: "", status: "Unknown" },
+  { id: 1, opponent: "Unknown", Type: 'Unknown', date: "", time: "Not scheduled", venue: "", status: "Unknown" },
   { id: 2, opponent: "Bangladesh", Type:'Friendlies', date: "Nov 13, 2025",  venue: "Bangabandhu Stadium, Bangladesh", status: "2-2 Draw" },
-  { id: 2, opponent: "Malaysia", Type: 'Asian cup Qualifer', date: "Nov 18, 2025", venue: "Bukit jalil Stadium, Malaysia", status: "0-1 Loss" },
-  { id: 1, opponent: "Laos",Type:"Asian cup Qualifer", date: "March  31, 2026", venue: "laos national Stadium, Laos", status: "0-1 Loss" },
-  
-  
+  { id: 3, opponent: "Malaysia", Type: 'Asian cup Qualifer', date: "Nov 18, 2025", venue: "Bukit jalil Stadium, Malaysia", status: "0-1 Loss" },
+  { id: 4, opponent: "Laos",Type:"Asian cup Qualifer", date: "March  31, 2026", venue: "laos national Stadium, Laos", status: "0-1 Loss" }, 
 ];
